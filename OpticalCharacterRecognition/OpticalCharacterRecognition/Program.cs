847e8f9cb732f4b81e6eeaa850bd63b688a0c479
Pedro Gouveia <58569642+pedrogouveia69@users.noreply.github.com>
2022-05-06 17:05:17 +0100

    first

---
+++ OpticalCharacterRecognition/OpticalCharacterRecognition/Program.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OpticalCharacterRecognition
+{
+    class Program
+    {
+        // Computer Vision subscription key and endpoint
+        static string subscriptionKey = "44c59a3b22f44f14abd9e906dbb8383d";
+        static string endpoint = "https://vision-ocr-api-test.cognitiveservices.azure.com/";
+        static void Main(string[] args)
+        {
+            var client = Authenticate(endpoint, subscriptionKey);
+
+            Console.WriteLine("Insert the image's URL: ");
+            string urlFile = Console.ReadLine();
+
+            if (Uri.IsWellFormedUriString(urlFile, UriKind.Absolute))
+                ReadFileUrl(client, urlFile).Wait();
+            else
+                Console.WriteLine("\nThe inserted URL is not valid.");
+        }
+
+        public static ComputerVisionClient Authenticate(string endpoint, string key)
+        {
+            return new ComputerVisionClient(new ApiKeyServiceClientCredentials(key))
+            { Endpoint = endpoint };
+        }
+
+        public static async Task ReadFileUrl(ComputerVisionClient client, string urlFile)
+        {
+            Console.WriteLine("\nReading...");
+
+            // Read text from URL
+            var textHeaders = await client.ReadAsync(urlFile);
+            // After the request, get the operation location (operation ID)
+            string operationLocation = textHeaders.OperationLocation;
+            Thread.Sleep(2000);
+
+            // Retrieve the URI where the extracted text will be stored from the Operation-Location header.
+            // We only need the ID and not the full URL
+            const int numberOfCharsInOperationId = 36;
+            string operationId = operationLocation.Substring(operationLocation.Length - numberOfCharsInOperationId);
+
+            // Extract the text
+            ReadOperationResult results;
+            Console.WriteLine("\nExtracting text from image...");
+            do
+            {
+                results = await client.GetReadResultAsync(Guid.Parse(operationId));
+            }
+            while (results.Status == OperationStatusCodes.Running ||
+                results.Status == OperationStatusCodes.NotStarted);
+
+            // Save results to .txt file
+            Console.WriteLine("\nWriting text to file...");
+
+            var lines = new List<string>();
+            foreach (var line in results.AnalyzeResult.ReadResults[0].Lines)
+                lines.Add(line.Text);
+
+            File.WriteAllLines("results.txt", lines);
+        }
+
+    }
+}
---
+++ TextSummarization/TextSummarization/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Azure;
+using Azure.AI.TextAnalytics;
+using System.IO;
+
+namespace TextSummarization
+{
+    class Program
+    {
+        private static readonly AzureKeyCredential credentials = new AzureKeyCredential("3430760e509643b285878d0ef9eb7d9d");
+        private static readonly Uri endpoint = new Uri("https://text-analytics-api-test.cognitiveservices.azure.com/");
+
+        static async Task TextSummarization(TextAnalyticsClient client, string text)
+        {
+            // Prepare analyze operation input. You can add multiple documents to this list and perform the same
+            // operation to all of them.
+            var batchInput = new List<string> { text };
+            var actions = new TextAnalyticsActions()
+            {
+                // Add the desired TextAnalytics API actions here
+                ExtractSummaryActions = new List<ExtractSummaryAction>() { new ExtractSummaryAction() },
+            };
+
+            // Start analysis process.
+            Console.WriteLine("\nREADING...");
+            var response = await client.StartAnalyzeActionsAsync(batchInput, actions);
+            await response.WaitForCompletionAsync();
+
+            // Cycle through the different TextAnalytics API actions          
+            await foreach (var action in response.Value)
+            {
+                foreach (var actionResult in action.ExtractSummaryResults)
+                {
+                    foreach (var summary in actionResult.DocumentsResults)
+                    {
+                        Console.WriteLine("\nSUMMARY:\n");
+                        string textSummary = "";
+                        foreach (var sentence in summary.Sentences)
+                        {
+                            Console.WriteLine(sentence.Text);
+                            textSummary += sentence.Text + "\n";
+                        }
+
+                        var res = await client.ExtractKeyPhrasesAsync(textSummary);
+                        Console.WriteLine("\nKEY WORDS:\n");
+                        foreach (var keyWord in res.Value)
+                        {                          
+                            Console.Write(keyWord + " | ");
+                        }
+
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("INSERT TEXT TO SUMMARIZE:\n");
+            string text = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(text))
+            {
+                Console.WriteLine("No text was inserted.");
+            }
+            else
+            {
+                var client = new TextAnalyticsClient(endpoint, credentials);
+                await TextSummarization(client, text);
+            }
+        }
+    }
+}
